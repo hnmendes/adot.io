657e2c64d9f2d356a34ac1e5728c7341a39d9bde
Henrique Nunes Mendes <henriquenunesti@gmail.com>
2018-03-01 16:12:31 -0300

    Atualização

--- views/pages/adocao.php
+++ views/pages/adocao.php
@@ -79,9 +79,7 @@
 
 </main>
 
-<!--Import jQuery before materialize.js-->
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+
 <script>
 $(document).ready(function() {
 	$('select').material_select();
